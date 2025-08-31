724a56f91c115ac04f70a4240457583e8c1cbe52
Marat Suvanaliev <suvanaliev.marat@betting-software.online>
2025-09-01 01:57:36 +0600

    publish task in amqp

--- app/services/task.py
+++ app/services/task.py
@@ -4,16 +4,22 @@ from app.core.db.connection import session
 from app.repositories.task import TaskRepository
 from app.models.task import TaskModel
 from app.schemas.task import TaskCreate
+from app.core.amqp.producer import TaskQueueProducer
 
 
 class TaskService:
     def __init__(self, session: AsyncSession):
         self.repository = TaskRepository(session)
+        self.producer = TaskQueueProducer()
 
     async def create_task(self, task: TaskCreate) -> TaskModel:
         new_task = await self.repository.create_task(
             type=task.type,
             payload=task.payload
+        )
+        await self.producer.publish_task(
+            task_type=task.type,
+            task_id=str(new_task.id)
         )
         return new_task
 
